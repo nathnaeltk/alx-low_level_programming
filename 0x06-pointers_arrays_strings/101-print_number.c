f38e2c649ff80316d4d14f03f97e6288af59ba14
nathnaeltk <87467768+nathnaeltk@users.noreply.github.com>
2022-03-24 15:02:53 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,26 @@
-#include "holberton.h"
+/*
+ * File: 101-print_number.c
+ * Auth: Nate
+ */
+
+#include "main.h"
 
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
-
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
 void print_number(int n)
 {
-unsigned int n1;
-
-n1 = n;
+unsigned int num = n;
 
 if (n < 0)
 {
 _putchar('-');
-n1 = -n;
+num = -num;
 }
 
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+if ((num / 10) > 0)
+print_number(num / 10);
+
+_putchar((num % 10) + '0');
 }
